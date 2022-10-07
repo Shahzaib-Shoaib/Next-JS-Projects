b7e7b4cc26c40ea8e9e36281f08fc1cddd54e9ae
Shahzaib-Shoaib <82258192+Shahzaib-Shoaib@users.noreply.github.com>
2022-10-07 15:57:03 +0500

    .

---
+++ directus-next/app/pages/_app.jsx
@@ -0,0 +1,17 @@
+import 'tailwindcss/tailwind.css'
+import { QueryClientProvider, QueryClient } from 'react-query'
+import '../styles/globals.css'
+
+const queryClient = new QueryClient()
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </>
+  )
+}
+
+export default MyApp
