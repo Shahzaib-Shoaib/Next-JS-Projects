2e891179fb2d5cceb38d022af484acce8b83241f
Shahzaib-Shoaib <82258192+Shahzaib-Shoaib@users.noreply.github.com>
2022-10-17 11:47:19 +0500

    .

--- razor-clone/src/pages/_app.tsx
+++ razor-clone/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router';
 import { ManagedUIContext } from '../contexts/ui.context';
 import ManagedModal from '../components/common/modal/managed-modal';
+import ManagedDrawer from '../components/common/drawer/managed-drawer';
 
 
 
