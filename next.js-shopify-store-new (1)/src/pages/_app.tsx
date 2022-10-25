ecb9032bff11f3a8eefc388547943466ecaa1275
Shahzaib-Shoaib <82258192+Shahzaib-Shoaib@users.noreply.github.com>
2022-10-25 14:09:00 +0500

    .

--- next.js-shopify-store-new (1)/src/pages/_app.tsx
+++ next.js-shopify-store-new (1)/src/pages/_app.tsx
@@ -1,37 +1,100 @@
-import type { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
-//import { ManagedUIContext } from '@contexts/ui.context';
-//import ManagedModal from '@components/common/modal/managed-modal';
-//import ManagedDrawer from '@components/common/drawer/managed-drawer';
-import { useEffect, useRef } from 'react';
-import { ToastContainer } from 'react-toastify';
-//import { ReactQueryDevtools } from 'react-query/devtools';
-import { appWithTranslation } from 'next-i18next';
+// import "@styles/globals.css";
+
+// import type { AppProps } from 'next/app';
+// import { useRouter } from 'next/router';
+// //import { ManagedUIContext } from '@contexts/ui.context';
+// //import ManagedModal from '@components/common/modal/managed-modal';
+// //import ManagedDrawer from '@components/common/drawer/managed-drawer';
+// import { useEffect, useRef } from 'react';
+// import { ToastContainer } from 'react-toastify';
+// //import { ReactQueryDevtools } from 'react-query/devtools';
+// import { appWithTranslation } from 'next-i18next';
+// import { DefaultSeo } from '@components/seo/default-seo';
+
+// import { getDirection } from '@utils/get-direction';
+
+// const Noop: React.FC = ({ children }) => <>{children}</>;
+
+// const MyApp = ({ Component, pageProps }: AppProps) => {
+  
+//   const router = useRouter();
+//   const dir = getDirection(router.locale);
+//   useEffect(() => {
+//     document.documentElement.dir = dir;
+//   }, [dir]);
+//   const Layout = (Component as any).Layout || Noop;
+
+//   return (
+//           <>
+//             <DefaultSeo />
+//             <Layout pageProps={pageProps}>
+//               <Component {...pageProps} key={router.route} />
+//             </Layout>
+//             <ToastContainer />
+//           </>
+//   );
+// };
+
+// export default appWithTranslation(MyApp);
+
+
+
+
+import "@styles/globals.css";
+//import "../styles/globals.css";
+import Navbar from "@components/Navbar";
+import Footer from "@components/layout/footer/footer";
+import { AppProps } from "next/app"
+import { FunctionComponent } from "react"
+import {ReactNode} from 'react'
 import { DefaultSeo } from '@components/seo/default-seo';
 
-import "../styles/globals.css";
-import { getDirection } from '@utils/get-direction';
+interface ReturnNode {
+  children: ReactNode |  ReactNode[];
+} 
+const Noop = ({children}:ReturnNode):JSX.Element => <>{children}</>
 
-const Noop: React.FC = ({ children }) => <>{children}</>;
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  
-  const router = useRouter();
-  const dir = getDirection(router.locale);
-  useEffect(() => {
-    document.documentElement.dir = dir;
-  }, [dir]);
-  const Layout = (Component as any).Layout || Noop;
+// page component and page props passed in 
+// Layout included in component object
+function MyApp({Component, pageProps}: AppProps & {Component: {Layout: FunctionComponent}}) {
+
+  // if no layout Noop returns just chldren in fragment
+  const Layout = Component.Layout ?? Noop
 
   return (
-          <>
-            <DefaultSeo />
-            <Layout pageProps={pageProps}>
-              <Component {...pageProps} key={router.route} />
-            </Layout>
-            <ToastContainer />
-          </>
-  );
-};
+    <div>
+      <DefaultSeo />
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer /> 
+     
+       {/* <Layout>
+      <Component {...pageProps } />
+      </Layout>  */}
 
-export default appWithTranslation(MyApp);
+    </div>
+  )
+}
+
+
+export default MyApp
+
+/*
+import "../styles/globals.css";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
+}
+
+export default MyApp;
+*/
+
