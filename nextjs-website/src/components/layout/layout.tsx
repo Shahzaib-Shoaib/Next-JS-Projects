16a9a99a14ea49bf7a780ffd97c224de3929cc13
Shahzaib-Shoaib <82258192+Shahzaib-Shoaib@users.noreply.github.com>
2022-11-25 22:42:07 +0500

    Update layout.tsx

--- nextjs-website/src/components/layout/layout.tsx
+++ nextjs-website/src/components/layout/layout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Footer from "@components/layout/footer/footer";
-import Nav from "@components/layout/header/nav";
 import BannerTop from "@components/common/banner-top";
 import Header from "@components/layout/header/header";
 // import Search from "@components/common/search";
