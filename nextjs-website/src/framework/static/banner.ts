edbce3781e8b79a9fa530badafb666f9d0cf2187
Shahzaib-Shoaib <82258192+Shahzaib-Shoaib@users.noreply.github.com>
2022-12-24 19:10:41 +0500

    Update banner.ts

--- nextjs-website/src/framework/static/banner.ts
+++ nextjs-website/src/framework/static/banner.ts
@@ -17,23 +17,23 @@ export const HomePage = [
     //     },
     //   },
     // },
-    {
-      id: 2,
-      title: 'Banner 2',
-      slug: '',
-      image: {
-        mobile: {
-          url: '/assets/images/banner/homepage-mobile-2.webp',
-          width: 375,
-          height: 220,
-        },
-        desktop: {
-          url: '/assets/images/banner/homepage-desktop-2.webp',
-          width: 1920,
-          height: 900,
-        },
-      },
-    },
+    // {
+    //   id: 2,
+    //   title: 'Banner 2',
+    //   slug: '',
+    //   image: {
+    //     mobile: {
+    //       url: '/assets/images/banner/homepage-mobile-2.webp',
+    //       width: 375,
+    //       height: 220,
+    //     },
+    //     desktop: {
+    //       url: '/assets/images/banner/homepage-desktop-2.webp',
+    //       width: 1920,
+    //       height: 900,
+    //     },
+    //   },
+    // },
     {
       id: 3,
       title: 'Banner 3',
